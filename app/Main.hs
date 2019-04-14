8b328d32e122e946751f11428d5a45fe3a77ab2e
Andrzej Swatowski <andrzejswatowski@outlook.com>
2019-04-14 22:46:04 +0200

    Mock of interpreter

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,31 @@
+{-# LANGUAGE PackageImports #-}
+
 module Main where
 
+import LexBeatle
+import ParBeatle
+import AbsBeatle
+import Interpreter
+
+import ErrM
+
+import Control.Monad.IO.Class
+import System.Console.Haskeline
+
+process :: String -> IO ()
+process line = do
+  let res = pProgram (myLexer line)
+  case res of
+    (Bad s) -> print "err"
+    (Ok s) -> print (interpretProg s)
+
 main :: IO ()
-main = putStrLn "no elo"
+main = runInputT defaultSettings loop
+    where
+       loop :: InputT IO ()
+       loop = do
+           minput <- getInputLine "λ "
+           case minput of
+               Nothing -> return ()
+               Just ":q" -> outputStrLn "Goodbye."
+               Just input -> (liftIO $ process input) >> loop
