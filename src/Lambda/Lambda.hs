135e82496f7eaa132b94234de862ce17c94cd418
Andrzej Swatowski <andrzejswatowski@outlook.com>
2019-04-16 19:31:01 +0200

    Reader to ReaderT Except

--- src/Lambda/Lambda.hs
+++ src/Lambda/Lambda.hs
@@ -1,8 +1,7 @@
 module Lambda.Lambda where
 
 import Control.Monad.Reader
--- import Control.Monad.Trans
--- import Control.Monad.Trans.Maybe
+import Control.Monad.Except
 
 import qualified Data.Map as Map
 
@@ -29,10 +28,10 @@ data Value = VInt Int | VBool Bool | VClos Expr ValMap
     deriving (Show)
 
 type ValMap = Map.Map Name Value
-type Env = Reader ValMap Value
+type Env = ReaderT ValMap (Except String) Value
 
-eval :: Expr -> Value
-eval expr = runReader (eval' expr) Map.empty
+eval :: Expr -> Either String Value
+eval expr = runExcept (runReaderT (eval' expr) Map.empty)
 
 eval' :: Expr -> Env
 eval' (Lit l) = case l of
@@ -41,7 +40,7 @@ eval' (Lit l) = case l of
 
 eval' (Var var) = do
     env <- ask
-    return $ env Map.! var
+    maybe (throwError $ "Unbound value " ++ var) return (Map.lookup var env)
 
 eval' e@(Lam _ _) = do
     env <- ask
@@ -50,31 +49,58 @@ eval' e@(Lam _ _) = do
 eval' (App e1 e2) = do
     eval1 <- eval' e1
     eval2 <- eval' e2
-    return $ apply eval1 eval2
+    either throwError return $ apply eval1 eval2
     where
         apply (VClos (Lam x e1) env) e2 =
-            runReader (eval' e1) (Map.insert x e2 env)
+            runExcept (runReaderT (eval' e1) (Map.insert x e2 env))
+        apply _ _ = throwError "First expression is not a function; it cannot be applied"
 
 eval' (BinOp op e1 e2) = do
     eval1 <- eval' e1
     eval2 <- eval' e2
-    return $ case op of
-        OpAdd -> opAdd eval1 eval2
-        OpMul -> opMul eval1 eval2
-        OpSub -> opSub eval1 eval2
-        OpDiv -> opDiv eval1 eval2
-        OpAnd -> opAnd eval1 eval2
-        OpOr  -> opOr eval1 eval2
+    either throwError return $ binOp eval1 eval2 op
     where
-        opAdd (VInt a) (VInt b)   = VInt $ a + b
-        opMul (VInt a) (VInt b)   = VInt $ a * b
-        opSub (VInt a) (VInt b)   = VInt $ a - b
-        opDiv (VInt a) (VInt b)   = VInt $ a `div` b
-        opAnd (VBool a) (VBool b) = VBool $ a && b
-        opOr  (VBool a) (VBool b) = VBool $ a || b
-
+        binOp :: Value -> Value -> BinOp -> Either String Value
+        binOp (VInt a) (VInt b) OpAdd = return . VInt $ a + b
+        binOp (VInt a) (VInt b) OpMul = return . VInt $ a * b
+        binOp (VInt a) (VInt b) OpSub = return . VInt $ a - b
+        binOp (VInt a) (VInt 0) OpDiv = throwError "Cannot divide by zero"
+        binOp (VInt a) (VInt b) OpDiv = return . VInt $ a `div` b
+        binOp _ _ OpAdd = throwError intTypeError
+        binOp _ _ OpMul = throwError intTypeError
+        binOp _ _ OpSub = throwError intTypeError
+        binOp _ _ OpDiv = throwError intTypeError
+        binOp (VBool a) (VBool b) OpAnd = return . VBool $ a && b
+        binOp  (VBool a) (VBool b) OpOr = return . VBool $ a || b
+        binOp _ _ OpAnd = throwError boolTypeError
+        binOp _ _ OpOr  = throwError boolTypeError
 
 eval' (UnOp op e) = do
-    (VBool b) <- eval' e
-    return $ case op of
-        OpNeg -> VBool $ not b
+    ev <- eval' e
+    either throwError return $ boolOp ev op
+    where 
+        boolOp :: Value -> UnOp -> Either String Value
+        boolOp (VBool b) OpNeg = return . VBool $ not b
+        boolOp _ _ = throwError boolTypeError
+
+
+typeError :: String -> String
+typeError t = "Expression was expected of type " ++ t
+
+intTypeError :: String
+intTypeError = typeError "int"
+
+boolTypeError :: String
+boolTypeError = typeError "bool"
+
+example1 :: Expr
+example1 = App (App (App (Lam "f" (Lam "g" (Lam "h" (App (App (Var "f") (App (App (Var "g") (Lit $ LInt 1)) (Lit $ LInt 2))) (App (App (Var "h") (Lit $ LInt 3)) (Lit $ LInt 4)))))) (Lam "x" (Lam "y" (BinOp OpAdd (Var "x") (Var "y"))))) (Lam "x" (Lam "y" (BinOp OpSub (Var "x") (Var "y"))))) (Lam "x" (Lam "y" (BinOp OpMul (Var "x") (Var "y"))))
+
+twice :: Expr
+twice = Lam "f" (Lam "x" (App (Var "f") (App (Var "f") (Var "x"))))
+
+not' :: Expr
+not' = Lam "x" (UnOp OpNeg (Var "x"))
+
+example2 :: Expr
+example2 = App (Lam "t" (App (App (Var "t") (not')) (Lit $ LBool True))) twice
