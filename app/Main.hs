d75f87d2593443a6396282223ad1b346e3ca8803
Andrzej Swatowski <andrzejswatowski@outlook.com>
2019-04-29 18:39:21 +0200

    Add fancy and warm welcome to REPL

--- app/Main.hs
+++ app/Main.hs
@@ -18,6 +18,18 @@ import qualified Data.Map as Map
 
 import qualified Lambda.Lambda as L
 
+title :: [String]
+title = 
+    [ "______               _    _       "
+    , "| ___ \\             | |  | |      "
+    , "| |_/ /  ___   __ _ | |_ | |  ___ "
+    , "| ___ \\ / _ \\ / _` || __|| | / _ \\"
+    , "| |_/ /|  __/| (_| || |_ | ||  __/"
+    , "\\____/  \\___| \\__,_| \\__||_| \\___|"
+    , " Beatle 0.1.0 REPL"
+    , "                                  " ]
+
+myLLexer :: String -> [Token]
 myLLexer = resolveLayout True . myLexer
 
 eitherFunc :: Either String [L.Value] -> IState [String]
@@ -35,13 +47,15 @@ process :: String -> IState ()
 process line = do
     let res = pLine (myLLexer line)
     case res of
-        (Bad s) -> liftIO $ putStrLn "err"
+        (Bad s) -> if null line then return () else liftIO $ putStrLn "err"
         (Ok s) -> (liftIO . putStr . unlines) =<< eitherFunc =<< interpretLine s
         -- (Ok s) -> print $ map eitherFunc (interpretLine s)
 
 main :: IO ()
-main = runInputT defaultSettings (runExceptT $ runStateT loop Map.empty) >>= rerun main
+main = (putStr . unlines $ title) >> run
     where
+        run :: IO ()
+        run = runInputT defaultSettings (runExceptT $ runStateT loop Map.empty) >>= rerun run
         loop :: IState ()
         loop = do
             minput <- lift $ lift $ getInputLine "λ "
@@ -51,4 +65,4 @@ main = runInputT defaultSettings (runExceptT $ runStateT loop Map.empty) >>= rer
                 Just input -> process input >> loop
         rerun :: IO () -> Either String b -> IO ()
         rerun f (Left err) = putStrLn ("Error: " ++ err) >> f
-        rerun f (Right _) = return ()
+        rerun _ (Right _) = return ()
